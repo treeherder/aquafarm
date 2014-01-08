4b92463194f15858c825424a5e6f1f35a3f9f5a6
treeherder <b.d.reddybest@gmail.com>
2014-01-08 05:00:53 -0800

    ready for testing
    cntlr is ready for beta testing!  hope things work out for the way i
    tried to work around having too many lines of code

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ growbot project materials
 <dl>
 <dt>data logging</dt>
 <dd>cntrlr  is a datalogging script for the arduino connected to our raspberry pi</dd>
+<dd>stores UNIXtime, float in tables by sensor name</dd>
 </dl>
+================================
+useful information:
+--------------------
+<dl>
+<dt>black  onewire address:</dt>
+<dd>`0x28, 0x5A, 0xF9, 0x36, 0x04, 0x00, 0x00, 0x75`</dd>
--- cntrlr.py
+++ cntrlr.py
@@ -1,47 +1,55 @@
 import time
 import serial
 import sqlite3 as sq
-class ctrl:
-  LUX = "a"
-  TEMP = "b"
-  PRESSURE = "c"
-	LIGHT_ON = "y"
-	LIGHT_OFF = "z"
+
+ctrl = dict( LUX = "a", 
+  TEMP = "b",
+  PRESSURE = "c",
+  PH = "d",
+  WATER_TEMP = "e",
+	LIGHT_ON = "y",
+	LIGHT_OFF = "z")
 	
 class Controller():
  
-  def __init__(self):
+  def __init__(self, cmnd):
     self.com = serial.Serial('/dev/ttyACM0', '57600', timeout = None)
+    self.cmnd = cmnd
     time.sleep(2)
- 
-  def get_lux(self):
-    self.com.write(ctrl.LUX) #get lux
+    
+  def poll_sensor (self, cmnd):  #use this method to get sensor data
+    self.com.write(self.cmnd) #send command byte
     self.com.flush()
     time.sleep(0.2)
-    return  float(self.com.readline())
- 
- 
-  def get_temp(self):
-    self.com.write(ctrl.TEMP)  # get temp in degrees centigrade
-    time.sleep(0.2)
-    return  float(self.com.readline())
+    return  float(self.com.readline()) #return value as a float
 
-  def get_press(self):
-    self.com.write(ctrl.PRESSURE)  # get pressure information in millibars
-    time.sleep(0.2)
-    return  float(self.com.readline())
-
-
+ def ctrl_device (self, cmnd):  #use this method to control lights, pump. heater
+    self.com.write(self.cmnd) #send command byte
+    self.com.flush()
+    
 
 class Keeper():
-  def __init__(self, data):
+  def __init__(self, sensor, data):
     self.con = sq.connect('sensors.db')
     self.data = data
-  def put_data(x):
+    self.sensor = sensor
+    control = Controller()
+  def put_data(self, data, sensor):
     with self.con:
       cur = con.cursor()
-      cur.execute('create table if not exists ' + table_name + ' (id integer)')
-      cur.execute("{0},{1}".format( time.time(), self.data))
+      cur.execute('create table if not exists ' + sensor + ' (time, data) ')
+      cur.execute("INSERT INTO {0} VALUES({1},{2}".format(sensor, time.time(), self.data))
       self.con.commit()
       self.con.close()
+  def collect_data(self):
+    for key, cmd in ctrl.items():
+      put_data(control.poll_sensor(cmd), key)
 
+
+datamkr = Keeper()
+
+if __name__ == '__main__':
+    datamkr = Keeper()
+    datamkr.collect_data()
+
+ 
