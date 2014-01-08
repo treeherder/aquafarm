20ea1b157f1da81f6b63547c3e580f3d657a66ed
Brendan Reddy-Best <breddybest@raspberrypi>
2014-01-08 07:31:16 -0800

    testing ready

--- cntrlr.py
+++ cntrlr.py
@@ -6,45 +6,37 @@ ctrl = dict( LUX = "a",
   TEMP = "b",
   PRESSURE = "c",
   PH = "d",
-  WATER_TEMP = "e",
-  LIGHT_ON = "y",
-  LIGHT_OFF = "z")
+  WATER_TEMP = "e")
 	
-class Controller():
- 
-  def __init__(self,):
+class Controller:
+  def __init__(self ):
     self.com = serial.Serial('/dev/ttyACM0', '57600', timeout = None)
     time.sleep(2)
-    
-  def poll_sensor (self, cmnd):  #use this method to get sensor data
+  def poll_sensor(self, cmnd):  #use this method to get sensor data
     self.com.write(cmnd) #send command byte
     self.com.flush()
     time.sleep(0.2)
     return  float(self.com.readline()) #return value as a float
-
-  def ctrl_device (self, cmnd):  
+  def ctrl_device  (cmnd):  
     #use this method to control lights, pump. heater
     self.com.write(cmnd) #send command byte
     self.com.flush()
-    
+cnt = Controller()
 
 class Keeper():
   def __init__(self):
     self.con = sq.connect('sensors.db')
   def put_data(self, data, sensor):
     with self.con:
-      cur = con.cursor()
-      cur.execute('create table if not exists ' + sensor + ' (time, data) ')
-      cur.execute("INSERT INTO {0} VALUES({1},{2}".format(sensor, time.time(), self.data))
+      cur = self.con.cursor()
+      cur.execute('create table if not exists ' + sensor + ' (time REAL, data REAL) ')
+      cur.execute("INSERT INTO "+sensor+" VALUES(?,?)", (time.time(), data))
       self.con.commit()
-      self.con.close()
   def collect_data(self):
-    controller = Controller()
     for key, cmd in ctrl.items():
-      self.put_data(controller.poll_sensor(cmd), key)
+      self.put_data(cnt.poll_sensor(cmd), key)
 
 
-datamkr = Keeper()
 
 if __name__ == '__main__':
     datamkr = Keeper()
